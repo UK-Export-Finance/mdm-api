84e9d94d15fa7f94d13c1d39a9c29d7098ce998b
Gabriel Ignat <gabriel.ignat@hotmail.com>
2023-02-20 11:11:15 +0000

    feat: added unit test

---
+++ src/modules/interest-rates/interest-rates.controller.spec.ts
@@ -0,0 +1,57 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import Chance from 'chance';
+
+import { InterestRatesController } from './interest-rates.controller';
+import { InterestRatesService } from './interest-rates.service';
+
+const chance = new Chance();
+
+describe('InterestRatesController', () => {
+  let interestRatesController: InterestRatesController;
+  let interestRatesService: InterestRatesService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [InterestRatesController],
+      providers: [
+        InterestRatesService,
+        {
+          provide: InterestRatesService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([
+              {
+                id: chance.natural(),
+                category: chance.word(),
+                subCategory: chance.word(),
+                termMonths: chance.natural(),
+                sourceTerm: chance.word(),
+                currency: chance.currency().code,
+                interestRate: chance.natural(),
+                bukKey: chance.word(),
+                effectiveFrom: chance.date({ string: true }),
+                effectiveTo: chance.date({ string: true }),
+                created: chance.date({ string: true }),
+                updated: chance.date({ string: true }),
+              },
+            ]),
+          },
+        },
+      ],
+    }).compile();
+
+    interestRatesController = app.get<InterestRatesController>(InterestRatesController);
+    interestRatesService = app.get<InterestRatesService>(InterestRatesService);
+  });
+
+  it('should be defined', () => {
+    expect(interestRatesController).toBeDefined();
+  });
+
+  describe('findAll()', () => {
+    it('should return all interest rates', () => {
+      interestRatesController.findAll();
+
+      expect(interestRatesService.findAll).toHaveBeenCalled();
+    });
+  });
+});
