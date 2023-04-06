692898fe8998c2c594fe66ad1850bec1a809cfd3
Gabriel Ignat <gabriel.ignat@hotmail.com>
2023-04-06 13:38:58 +0100

    fix: removed transform for currency dates

--- src/modules/currencies/entities/currency.entity.ts
+++ src/modules/currencies/entities/currency.entity.ts
@@ -1,7 +1,5 @@
 import { ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
-import { format } from 'date-fns';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({
@@ -24,22 +22,18 @@ export class CurrencyEntity {
 
   @Column({ name: 'DATE_CREATED_DATETIME', type: 'timestamp' })
   @ApiProperty({ example: '2017-03-06T16:20:17' })
-  @Transform(({ value }) => format(value, "yyyy-MM-dd'T'HH:mm:ss"))
   created: Date;
 
   @Column({ name: 'DATE_LAST_UPDATED_DATETIME', type: 'timestamp' })
   @ApiProperty({ example: '2017-03-06T16:20:16' })
-  @Transform(({ value }) => format(value, "yyyy-MM-dd'T'HH:mm:ss"))
   updated: Date;
 
   @Column({ name: 'EFFECTIVE_FROM_DATETIME', type: 'timestamp' })
   @ApiProperty({ example: '2017-03-07T10:44:47' })
-  @Transform(({ value }) => format(value, "yyyy-MM-dd'T'HH:mm:ss"))
   effectiveFrom: Date;
 
   @Column({ name: 'EFFECTIVE_TO_DATETIME', type: 'timestamp' })
   @ApiProperty({ example: '9999-12-31T00:00:00' })
-  @Transform(({ value }) => format(value, "yyyy-MM-dd'T'HH:mm:ss"))
   effectiveTo: Date;
 
   @Column({ name: 'ACBS_CURRENCY_CODE' })
