38e9e9e978fc330e3dbef2c7d49ee640804d0367
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2023-05-26 09:36:03 +0100

    feat(APIM-261): individual database hosts (#207)
    ## Introduction
    Separate database connection is one of the production environment
    pre-requisite.
    
    ## Resolution
    * Updated `.env.sample`, `deployment.yml`, `docker-compose.yml` and
    `database.config.ts`.
    * DB table and SP ACL has been documented and shared internally.
    
    Co-authored-by: Abhi Markan <abhi.markan@ukexportfinance.gov.uk>

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,28 +6,28 @@ export default registerAs(
   'database',
   (): Record<string, any> => ({
     mssql_mdm: {
-      host: process.env.DATABASE_HOST,
+      host: process.env.DATABASE_MDM_HOST,
       port: +process.env.DATABASE_PORT,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       name: process.env.DATABASE_MDM_NAME,
     },
     mssql_cedar: {
-      host: process.env.DATABASE_HOST,
+      host: process.env.DATABASE_CEDAR_HOST,
       port: +process.env.DATABASE_PORT,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       name: process.env.DATABASE_CEDAR_NAME,
     },
     mssql_number_generator: {
-      host: process.env.DATABASE_HOST,
+      host: process.env.DATABASE_NUMBER_GENERATOR_HOST,
       port: +process.env.DATABASE_PORT,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       name: process.env.DATABASE_NUMBER_GENERATOR_NAME,
     },
     mssql_cis: {
-      host: process.env.DATABASE_HOST,
+      host: process.env.DATABASE_CIS_HOST,
       port: +process.env.DATABASE_PORT,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
