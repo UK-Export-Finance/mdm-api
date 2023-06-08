bafcddc0ce1c2cce6fc7a508dc55f6abd1e51c6e
Abhi Markan <abhi.markan@ukexportfinance.gov.uk>
2023-06-08 15:40:07 +0100

    fix(remove-env): removed API_KEY_STRATEGY env variable

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -14,7 +14,6 @@ export default registerAs(
 
     globalPrefix: '/api',
     port: process.env.HTTP_PORT ? Number.parseInt(process.env.HTTP_PORT, 10) : 3003,
-    apiKey: process.env.API_KEY,
-    apiKeyStrategy: process.env.API_KEY_STRATEGY,
+    apiKey: process.env.API_KEY
   }),
 );
