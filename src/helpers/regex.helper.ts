2a45f7f216eb122b571ccd00c4216b02a4a24e20
Alex Bramhill <abramhill@ukexportfinance.gov.uk>
2023-07-12 10:34:28 +0100

    fix(APIM-531): fix swagger regex field

--- src/helpers/regex.helper.ts
+++ src/helpers/regex.helper.ts
@@ -1,4 +1,4 @@
 // This helper function is used to convert the regex pattern to a string so that it can be used in the swagger documentation.
 export const regexToString = (regex: RegExp): string => {
-  return regex.toString().replace(/^\/|\/$/g, '');
+  return regex.source;
 };
