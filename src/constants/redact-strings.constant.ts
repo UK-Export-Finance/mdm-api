bfeaa1e190b1872e346a5866a5df778a859f2f0f
Abhi Markan <32125108+abhi-markan@users.noreply.github.com>
2023-09-30 16:07:24 +0100

    feat(ami): migration to Azure AMI (#514)
    ## Introduction
    Migrate to Azure AMI
    
    ## Resolution
    * Updated `Docker`, `.env.sample`, `database.config.ts` and
    `redact.string.ts` configuration.
    * Updated GitHub secrets.
    
    ## Miscellaneous
    * Dependencies updates
    
    ---------
    
    Co-authored-by: Abhi Markan <abhi.markan@ukexportfinance.gov.uk>

--- src/constants/redact-strings.constant.ts
+++ src/constants/redact-strings.constant.ts
@@ -2,8 +2,5 @@ export const REDACT_STRINGS = [
   { searchValue: /(Login failed for user ').*(')/g, replaceValue: '$1[Redacted]$2' },
   { searchValue: process.env.DATABASE_USERNAME, replaceValue: '[Redacted]' },
   { searchValue: process.env.DATABASE_PASSWORD, replaceValue: '[Redacted]' },
-  { searchValue: process.env.DATABASE_MDM_HOST, replaceValue: '[RedactedDomain]' },
-  { searchValue: process.env.DATABASE_CEDAR_HOST, replaceValue: '[RedactedDomain]' },
-  { searchValue: process.env.DATABASE_NUMBER_GENERATOR_HOST, replaceValue: '[RedactedDomain]' },
-  { searchValue: process.env.DATABASE_CIS_HOST, replaceValue: '[RedactedDomain]' },
+  { searchValue: process.env.DATABASE_HOST, replaceValue: '[RedactedDomain]' },
 ];
