5cc241295bed81b5a16f158fc68bdda8383b919d
Audrius <audrius.vaitonis@ukexportfinance.gov.uk>
2024-04-25 10:37:02 +0100

    Update src/modules/geospatial/geospatial.controller.test.ts
    Co-authored-by: oscar-richardson-softwire <116292912+oscar-richardson-softwire@users.noreply.github.com>

--- src/modules/geospatial/geospatial.controller.test.ts
+++ src/modules/geospatial/geospatial.controller.test.ts
@@ -37,7 +37,7 @@ describe('GeospatialController', () => {
 
       const response = await controller.getAddressesByPostcode({ postcode });
 
-      expect(geospatialServiceGetAddressesByPostcode).toHaveBeenCalled();
+      expect(geospatialServiceGetAddressesByPostcode).toHaveBeenCalledTimes(1);
       expect(response).toEqual(getAddressByPostcodeResponse[0]);
     });
 
