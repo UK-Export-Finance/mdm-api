4605ade294585695dfa707b082852dcae09b602f
oscar-richardson-softwire <oscar.richardson@softwire.com>
2024-05-03 10:51:27 +0100

    feat(DTFS2-7121): add unit tests for Companies Controller

--- src/modules/companies/companies.controller.test.ts
+++ src/modules/companies/companies.controller.test.ts
@@ -1,33 +1,48 @@
 import { GetCompanyGenerator } from '@ukef-test/support/generator/get-company-generator';
 import { RandomValueGenerator } from '@ukef-test/support/generator/random-value-generator';
-import { resetAllWhenMocks } from 'jest-when'; // eslint-disable-line unused-imports/no-unused-vars
+import { resetAllWhenMocks, when } from 'jest-when';
 
 import { CompaniesController } from './companies.controller';
 import { CompaniesService } from './companies.service';
 
 describe('CompaniesController', () => {
   let companiesServiceGetCompanyByRegistrationNumber: jest.Mock;
-
-  let controller: CompaniesController; // eslint-disable-line unused-imports/no-unused-vars
+  let controller: CompaniesController;
 
   const valueGenerator = new RandomValueGenerator();
-  // eslint-disable-next-line unused-imports/no-unused-vars
-  const { getCompanyResponse } = new GetCompanyGenerator(valueGenerator).generate({
-    numberToGenerate: 2,
-  });
 
   beforeEach(() => {
     resetAllWhenMocks();
-    const companiesService = new CompaniesService(null, null);
+
+    const companiesService = new CompaniesService(null);
     companiesServiceGetCompanyByRegistrationNumber = jest.fn();
     companiesService.getCompanyByRegistrationNumber = companiesServiceGetCompanyByRegistrationNumber;
 
     controller = new CompaniesController(companiesService);
   });
 
-  it('should be defined', () => {
-    expect(CompaniesController).toBeDefined();
-  });
+  describe('getCompanyByRegistrationNumber', () => {
+    const testRegistrationNumber = '00000001';
+    const { getCompanyResponse } = new GetCompanyGenerator(valueGenerator).generate({
+      numberToGenerate: 1,
+      registrationNumber: testRegistrationNumber,
+    });
 
-  describe('getCompanyByRegistrationNumber()', () => {});
+    it('calls getCompanyByRegistrationNumber on the CompaniesService with the registration number', async () => {
+      when(companiesServiceGetCompanyByRegistrationNumber).calledWith(testRegistrationNumber).mockReturnValueOnce(getCompanyResponse);
+
+      await controller.getCompanyByRegistrationNumber({ registrationNumber: testRegistrationNumber });
+
+      expect(companiesServiceGetCompanyByRegistrationNumber).toHaveBeenCalledTimes(1);
+      expect(companiesServiceGetCompanyByRegistrationNumber).toHaveBeenCalledWith(testRegistrationNumber);
+    });
+
+    it('returns the company from the service', async () => {
+      when(companiesServiceGetCompanyByRegistrationNumber).calledWith(testRegistrationNumber).mockReturnValueOnce(getCompanyResponse);
+
+      const response = await controller.getCompanyByRegistrationNumber({ registrationNumber: testRegistrationNumber });
+
+      expect(response).toEqual(getCompanyResponse);
+    });
+  });
 });
--- src/modules/companies/companies.module.ts
+++ src/modules/companies/companies.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CompaniesHouseModule } from '@ukef/helper-modules/companies-house/companies-house.module';
-import { SectorIndustriesModule } from '@ukef/modules/sector-industries/sector-industries.module';
 
 import { CompaniesController } from './companies.controller';
 import { CompaniesService } from './companies.service';
 
 @Module({
-  imports: [CompaniesHouseModule, SectorIndustriesModule],
+  imports: [CompaniesHouseModule],
   controllers: [CompaniesController],
   providers: [CompaniesService],
 })
