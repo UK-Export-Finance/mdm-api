fd5f078e5b8b036bfef7f0461de12f2853c1b228
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2024-07-31 10:21:09 +0100

    fix(company-porta): removed JSDoc type declaration

--- src/helpers/response-status.helper.ts
+++ src/helpers/response-status.helper.ts
@@ -3,19 +3,19 @@ import { isAxiosError } from 'axios';
 
 /**
  * Checks if the error object is an AxiosError and has a response status matching the specified status.
- * @param {Object} options - An object containing the error and status.
- * @param {Error} options.error - The error object to check.
- * @param {HttpStatus} options.status - The status to compare against.
- * @returns {boolean} - Returns true if the error is an AxiosError and has a matching response status, otherwise false.
+ * @param options - An object containing the error and status.
+ * @param options.error - The error object to check.
+ * @param options.status - The status to compare against.
+ * @returns - Returns true if the error is an AxiosError and has a matching response status, otherwise false.
  */
 export const statusCheck = ({ error, status }: { error: Error; status: HttpStatus }): boolean => isAxiosError(error) && error.response?.status === status;
 
 /**
  * Validates the error response against the specified search string.
- * @param {Object} options - An object containing the error and search string.
- * @param {Error} options.error - The error object to validate.
- * @param {string} options.search - The search string to compare against the error message.
- * @returns {boolean} - Returns true if the error response contains the search string, otherwise false.
+ * @param options - An object containing the error and search string.
+ * @param options.error - The error object to validate.
+ * @param options.search - The search string to compare against the error message.
+ * @returns - Returns true if the error response contains the search string, otherwise false.
  */
 export const messageCheck = ({ error, search }: { error: Error; search: string }): boolean => {
   if (!isAxiosError(error) || !error?.response?.data) {
