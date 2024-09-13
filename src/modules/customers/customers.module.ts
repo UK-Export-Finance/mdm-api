7e5da7f640c19b95f4b7fec3d8dcb39662318161
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-09-13 11:25:23 +0100

    feat: wip add salesforce service and module bypassing self-signed issue for now, using informatica config template

--- src/modules/customers/customers.module.ts
+++ src/modules/customers/customers.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { InformaticaModule } from '@ukef/modules/informatica/informatica.module';
+import { SalesforceModule } from  '@ukef/modules/salesforce/salesforce.module';
 
 import { CustomersController } from './customers.controller';
 import { CustomersService } from './customers.service';
 
 @Module({
-  imports: [InformaticaModule],
+  imports: [InformaticaModule, SalesforceModule],
   controllers: [CustomersController],
   providers: [CustomersService],
 })
