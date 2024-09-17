a5d193a536ebc2194dc9df3cb1b0241953c2943e
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-09-17 18:31:52 +0100

    feat: add custom sf error objects

---
+++ src/modules/salesforce/exception/salesforce.exception.ts
@@ -0,0 +1,10 @@
+export class SalesforceException extends Error {
+    constructor(
+      message: string,
+      public readonly innerError?: Error,
+    ) {
+      super(message);
+      this.name = this.constructor.name;
+    }
+  }
+  
