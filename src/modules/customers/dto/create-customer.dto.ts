b055538f3add631801c097d91926056bee14df5f
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-09-24 15:35:54 +0100

    feat: add unrestricted mvp fields

--- src/modules/customers/dto/create-customer.dto.ts
+++ src/modules/customers/dto/create-customer.dto.ts
@@ -2,8 +2,33 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCustomerDto {
-  @ApiProperty({ description: 'Customer name' })
+  @ApiProperty({ description: 'Account Name' })
   @IsString()
   @IsNotEmpty()
   Name: string;
+
+  @ApiProperty({ description: 'Trading Country' })
+  @IsString()
+  @IsNotEmpty()
+  BillingCountry: string;
+  
+  @ApiProperty({ description: 'Trading Street' })
+  @IsString()
+  @IsNotEmpty()
+  BillingStreet: string;
+
+  @ApiProperty({ description: 'Trading City' })
+  @IsString()
+  @IsNotEmpty()
+  BillingCity: string;
+
+  @ApiProperty({ description: 'Trading Zip/Postal Code' })
+  @IsString()
+  @IsNotEmpty()
+  BillingPostalCode: string;
+
+  @ApiProperty({ description: 'D&B Number' })
+  @IsString()
+  @IsNotEmpty()
+  D_B_Number__c: string;
 }
