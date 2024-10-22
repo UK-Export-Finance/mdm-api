18d0b2412d0ce4fa414e45f589f9b81f8d5b67ae
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-10-22 12:28:21 +0100

    feat: add companies house number to POST

--- src/modules/customers/dto/create-customer.dto.ts
+++ src/modules/customers/dto/create-customer.dto.ts
@@ -11,4 +11,9 @@ export class CreateCustomerDto {
   @IsString()
   @IsNotEmpty()
   D_B_Number__c: string;
+
+  @ApiProperty({ description: 'Companies House Number' })
+  @IsString()
+  @IsNotEmpty()
+  Company_Registration_Number__c: string;
 }
