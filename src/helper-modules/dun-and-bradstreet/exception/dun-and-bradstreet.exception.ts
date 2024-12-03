37c46ff126142defc9bcae1d453d4be9622decd4
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-12-03 16:37:23 +0000

    refactor: newlines

--- src/helper-modules/dun-and-bradstreet/exception/dun-and-bradstreet.exception.ts
+++ src/helper-modules/dun-and-bradstreet/exception/dun-and-bradstreet.exception.ts
@@ -1,9 +1,9 @@
 export class DunAndBradstreetException extends Error {
-    constructor(
-      message: string,
-      public readonly innerError?: Error,
-    ) {
-      super(message);
-      this.name = this.constructor.name;
-    }
+  constructor(
+    message: string,
+    public readonly innerError?: Error,
+  ) {
+    super(message);
+    this.name = this.constructor.name;
   }
+}
