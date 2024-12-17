041124cf54874c9c40896b64573ee6e9d4a1c633
Tony Barnes <tony@tonybarnes.me>
2024-12-17 10:03:35 +0000

    feat(APIM-609): companies house - extend returned fields (#1089)
    ## Introduction :pencil2:
    In order for Companies House to be consumed by other products/services,
    the integration needs to return some additional fields.
    
    Note: unfortunately, some methods in the Companies House service do not
    have unit test coverage as they are private methods. I would like to add
    unit tests for all private methods (likely needing to make them public
    or extracted into helper functions), but this would be a bigger change
    than introducing additional fields.
    
    ## Resolution :heavy_check_mark:
    - Update Companies House types.
    - Update `CompaniesService` to return the following additional data:
      - `accounts` object
      - `dateOfCreation`
      - `registeredAddress.careOf`
      - `registeredAddress.premises`
      - `registeredAddress.region`
    - Updated test generator shapes.
    
    # Todo ❗ 
    * Ensure tests are DRY and centralised helpers for types, DTO and
    interfaces.
    
    ---------
    
    Co-authored-by: Abhi Markan <amarkan>

--- src/helper-modules/companies-house/dto/get-company-companies-house-response.dto.ts
+++ src/helper-modules/companies-house/dto/get-company-companies-house-response.dto.ts
@@ -45,9 +45,12 @@ export type GetCompanyCompaniesHouseResponse = {
     address_line_1: string;
     address_line_2?: string;
     address_line_3?: string;
+    care_of?: string;
+    country: string;
     locality: string;
     postal_code: string;
-    country: string;
+    premises?: string;
+    region?: string;
   };
   registered_office_is_in_dispute: boolean;
   sic_codes: string[];
