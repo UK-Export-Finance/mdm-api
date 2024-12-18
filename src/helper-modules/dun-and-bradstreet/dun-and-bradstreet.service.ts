5a5d86937afc083d4c03d338c9bb3ee8cf66a5e8
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2024-12-18 15:39:07 +0000

    refactor: snake -> camel casing

--- src/helper-modules/dun-and-bradstreet/dun-and-bradstreet.service.ts
+++ src/helper-modules/dun-and-bradstreet/dun-and-bradstreet.service.ts
@@ -10,22 +10,22 @@ import { createWrapDunAndBradstreetHttpGetErrorCallback } from './wrap-dun-and-b
 @Injectable()
 export class DunAndBradstreetService {
   private readonly httpClient: HttpClient;
-  private readonly encoded_key: string;
+  private readonly encodedKey: string;
 
   constructor(httpService: HttpService, configService: ConfigService) {
     this.httpClient = new HttpClient(httpService);
     const { key } = configService.get<DunAndBradstreetConfig>(DUN_AND_BRADSTREET.CONFIG.KEY);
-    this.encoded_key = key;
+    this.encodedKey = key;
   }
 
   async getDunAndBradstreetNumberByRegistrationNumber(registrationNumber: string): Promise<string> {
     const path = `/v1/match/cleanseMatch?countryISOAlpha2Code=GB&registrationNumber=${registrationNumber}`;
-    const access_token = await this.getAccessToken();
+    const accessToken = await this.getAccessToken();
 
     const { data } = await this.httpClient.get<any>({
       path,
       headers: {
-        Authorization: 'Bearer ' + access_token,
+        Authorization: 'Bearer ' + accessToken,
       },
       onError: createWrapDunAndBradstreetHttpGetErrorCallback({
         messageForUnknownError: 'Failed to get response from Dun and Bradstreet API',
@@ -43,7 +43,7 @@ export class DunAndBradstreetService {
         grant_type: 'client_credentials',
       },
       headers: {
-        Authorization: 'Basic ' + this.encoded_key,
+        Authorization: 'Basic ' + this.encodedKey,
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       onError: createWrapDunAndBradstreetHttpGetErrorCallback({
@@ -51,6 +51,6 @@ export class DunAndBradstreetService {
         knownErrors: [],
       }),
     });
-    return response.data.access_token;
+    return response.data.accessToken;
   }
 }
