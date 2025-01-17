33e3a7b16d451e16fcf351182efb9b67adf3cca5
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2025-01-17 09:17:30 +0000

    refactor: lint

--- src/modules/customers/dto/dtfs-customer.dto.ts
+++ src/modules/customers/dto/dtfs-customer.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, MinLength, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class DTFSCustomerDto {
   @ApiProperty({ description: 'Company Registration Number', minLength: 8, maxLength: 10 })
