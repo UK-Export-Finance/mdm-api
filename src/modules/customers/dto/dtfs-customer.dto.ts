8af2e1f85eb2758c50ce2c0f456a796cfc3bdd6a
Nat Dean-Lewis <ndlewis@ukexportfinance.gov.uk>
2025-02-06 17:15:50 +0000

    feat: make PoD optional

--- src/modules/customers/dto/dtfs-customer.dto.ts
+++ src/modules/customers/dto/dtfs-customer.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
 
 export class DTFSCustomerDto {
   @ApiProperty({ description: 'Company Registration Number', minLength: 8, maxLength: 10 })
@@ -15,6 +15,7 @@ export class DTFSCustomerDto {
   companyName: string;
 
   @ApiProperty({ description: 'Probability of Default' })
+  @IsOptional()
   @IsNumber()
   @IsNotEmpty()
   @Min(0)
