45fbdf26abc435ae7f028a168fb2784f56cd9046
Abhi Markan <amarkan>
2025-02-10 14:33:58 +0000

    fix(error): added globalexceptional handling

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,22 @@ import { REDACT_STRINGS } from './constants';
 import { ConsoleLoggerWithRedact } from './logging/console-logger-with-redact';
 
 const main = async () => {
-  // If REDACT_LOGS is true use ConsoleLoggerWithRedact. ConsoleLoggerWithRedact is used just if `NestFactory.create` fails completely.
-  // If `NestFactory.create` doesn't fail completely, then buffered logs are passed to PinoLogger. NestLogger and ConsoleLoggerWithRedact are not used.
-  const logger = process.env.REDACT_LOGS !== 'false' ? new ConsoleLoggerWithRedact(REDACT_STRINGS) : new NestLogger();
-  const nestApp: NestApplication = await NestFactory.create(MainModule, { logger, bufferLogs: true });
-  const app = new App(nestApp);
+  try {
+    // If REDACT_LOGS is true use ConsoleLoggerWithRedact. ConsoleLoggerWithRedact is used just if `NestFactory.create` fails completely.
+    // If `NestFactory.create` doesn't fail completely, then buffered logs are passed to PinoLogger. NestLogger and ConsoleLoggerWithRedact are not used.
+    const logger = process.env.REDACT_LOGS !== 'false' ? new ConsoleLoggerWithRedact(REDACT_STRINGS) : new NestLogger();
+    const nestApp: NestApplication = await NestFactory.create(MainModule, { logger, bufferLogs: true });
+    const app = new App(nestApp);
 
-  logger.log(`==========================================================`);
-  logger.log(`Main app will serve on PORT ${app.port}`, 'MainApplication');
-  logger.log(`==========================================================`);
+    logger.log(`==========================================================`);
+    logger.log(`Main app will serve on PORT ${app.port}`, 'MainApplication');
+    logger.log(`==========================================================`);
 
-  return app.listen();
+    return app.listen();
+  } catch (error) {
+    console.error('❌ An error was encountered %o', error);
+    return false;
+  }
 };
 
 main();
