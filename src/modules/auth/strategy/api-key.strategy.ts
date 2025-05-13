9fa4b42260a9fd16bf17160fbcc5f17d66b333ac
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2025-05-13 09:19:32 +0100

    fix(major-11-all): dependencies updates

--- src/modules/auth/strategy/api-key.strategy.ts
+++ src/modules/auth/strategy/api-key.strategy.ts
@@ -6,6 +6,25 @@ import { HeaderAPIKeyStrategy } from 'passport-headerapikey';
 
 import { AuthService } from '../auth.service';
 
+/**
+ * Strategy for API key authentication using the `HeaderAPIKeyStrategy` from Passport.
+ * This strategy validates API keys provided in the request headers.
+ *
+ * @class ApiKeyStrategy
+ * @extends {PassportStrategy(HeaderAPIKeyStrategy, AUTH.STRATEGY)}
+ *
+ * @constructor
+ * @param {AuthService} authService - Service for handling authentication logic.
+ * @param {ConfigService} configService - Service for accessing application configuration.
+ *
+ * @method validate
+ * Validates the provided API key.
+ * Since `@nestjs/passport: 11.0.5`, `validate` is an abstract method
+ * which should be implemented by the derived class, unless an abstract class.
+ * @param {string} key - The API key extracted from the request header.
+ * @returns {Promise<boolean>} - Resolves to `true` if the API key is valid.
+ * @throws {UnauthorizedException} - Thrown if the API key is invalid.
+ */
 @Injectable()
 export class ApiKeyStrategy extends PassportStrategy(HeaderAPIKeyStrategy, AUTH.STRATEGY) {
   constructor(
@@ -13,12 +32,16 @@ export class ApiKeyStrategy extends PassportStrategy(HeaderAPIKeyStrategy, AUTH.
     private readonly configService: ConfigService,
   ) {
     const headerKeyApiKey: string = configService.get<string>('app.apiKeyStrategy');
-    super({ header: headerKeyApiKey, prefix: '' }, true, (apiKey: string, done: (arg0: UnauthorizedException, arg1: boolean) => void) => {
-      const hasValidKey = this.authService.validateApiKey(apiKey);
-      if (hasValidKey) {
-        return done(null, true);
-      }
-      done(new UnauthorizedException(), null);
-    });
+    super({ header: headerKeyApiKey, prefix: '' }, true);
+  }
+
+  validate(key: string): boolean {
+    const valid = this.authService.validateApiKey(key);
+
+    if (!valid) {
+      throw new UnauthorizedException('Invalid API key has been supplied');
+    }
+
+    return true;
   }
 }
