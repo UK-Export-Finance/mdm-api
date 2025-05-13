0197329be82f416c06fa1860c14d3153b954904e
Abhi Markan <amarkan@ukexportfinance.gov.uk>
2025-05-13 11:39:23 +0100

    fix(major-11-all): fixed env variable test cases for auth service

--- src/modules/auth/auth.service.test.ts
+++ src/modules/auth/auth.service.test.ts
@@ -2,13 +2,21 @@ import { ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
 import Chance from 'chance';
+import dotenv from 'dotenv';
 
 import { AuthService } from './auth.service';
 import { ApiKeyStrategy } from './strategy/api-key.strategy';
 
+dotenv.config();
+
 describe('AuthService', () => {
   let authService: AuthService;
+  let configService: ConfigService;
+
   const chance = new Chance();
+  const { API_KEY } = process.env;
+
+  const invalidKeys = [null, undefined, '', 'invalid', chance.word()];
 
   beforeAll(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -19,27 +27,32 @@ describe('AuthService', () => {
     authService = app.get<AuthService>(AuthService);
   });
 
+  beforeEach(() => {
+    configService = {
+      get: jest.fn().mockReturnValue(API_KEY),
+    } as any;
+
+    authService = new AuthService(configService);
+  });
+
   it('should be defined', () => {
+    // Assert
     expect(authService).toBeDefined();
   });
 
-  it('should return `false` when API Key is invalid', () => {
-    const key = chance.word();
+  it.each(invalidKeys)('should return `false` when API Key is %s', (key) => {
+    // Act
     const result = authService.validateApiKey(key);
 
-    expect(result).toBe(false);
-  });
-
-  it('should return `false` when API Key is not provided', () => {
-    const result = authService.validateApiKey('');
-
+    // Assert
     expect(result).toBe(false);
   });
 
   it('should return `true` when API Key is valid', () => {
-    const { API_KEY } = process.env;
+    // Act
     const result = authService.validateApiKey(API_KEY);
 
+    // Assert
     expect(result).toBe(true);
   });
 });
