f1109e9b9db39f09a07214f3d90ed7f81f5d3a24
Abhi Markan <amarkan>
2025-08-11 13:19:00 +0100

    feat(DTFS2-8059): updated snapshot

--- src/modules/customers/dto/dtfs-customer.dto.ts
+++ src/modules/customers/dto/dtfs-customer.dto.ts
@@ -3,19 +3,28 @@ import { EXAMPLES } from '@ukef/constants';
 import { IsNotEmpty, IsNumber, IsOptional, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
 
 export class DTFSCustomerDto {
-  @ApiProperty({ description: 'Company registration number', example: EXAMPLES.CUSTOMER.COMPANYREG })
+  @ApiProperty({
+    description: 'Company registration number',
+    example: EXAMPLES.CUSTOMER.COMPANYREG,
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
   @MaxLength(10)
   companyRegistrationNumber: string;
 
-  @ApiProperty({ description: 'Company name', example: EXAMPLES.CUSTOMER.NAME })
+  @ApiProperty({
+    description: 'Company name',
+    example: EXAMPLES.CUSTOMER.NAME,
+  })
   @IsString()
   @IsNotEmpty()
   companyName: string;
 
-  @ApiProperty({ description: 'Probability of default', example: EXAMPLES.CUSTOMER.PROBABILITY_OF_DEFAULT })
+  @ApiProperty({
+    description: 'Probability of default',
+    example: EXAMPLES.CUSTOMER.PROBABILITY_OF_DEFAULT,
+  })
   @IsOptional()
   @IsNumber()
   @IsNotEmpty()
@@ -23,21 +32,33 @@ export class DTFSCustomerDto {
   @Max(100)
   probabilityOfDefault?: number;
 
-  @ApiProperty({ description: 'UK entity', example: EXAMPLES.CUSTOMER.UK_ENTITY })
+  @ApiProperty({
+    description: 'UK entity',
+    example: EXAMPLES.CUSTOMER.UK_ENTITY,
+  })
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   @MinLength(1)
   @MaxLength(3)
   ukEntity?: string;
 
-  @ApiProperty({ description: 'UKEF industry identifier code', example: EXAMPLES.CUSTOMER.UK_INDUSTRY_NAME })
+  @ApiProperty({
+    description: 'UKEF industry identifier code',
+    example: EXAMPLES.CUSTOMER.UK_INDUSTRY_NAME,
+  })
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   @MinLength(1)
   @MaxLength(200)
   ukefIndustryName?: string;
 
-  @ApiProperty({ description: 'UKEF industry sector identifier code', example: EXAMPLES.CUSTOMER.UK_INDUSTRY_SECTOR_NAME })
+  @ApiProperty({
+    description: 'UKEF industry sector identifier code',
+    example: EXAMPLES.CUSTOMER.UK_INDUSTRY_SECTOR_NAME,
+  })
+  @IsOptional()
   @IsString()
   @IsNotEmpty()
   @MinLength(1)
