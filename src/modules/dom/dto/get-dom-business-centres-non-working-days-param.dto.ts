6f523433631ab13ff40113883eac10b77625410d
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2025-09-04 13:03:54 +0100

    feat(GIFT-15077): remove commented code

--- src/modules/dom/dto/get-dom-business-centres-non-working-days-param.dto.ts
+++ src/modules/dom/dto/get-dom-business-centres-non-working-days-param.dto.ts
@@ -7,9 +7,6 @@ export class GetDomBusinessCentresNonWorkingDaysParamDto {
     required: true,
     example: `${EXAMPLES.DOM.BUSINESS_CENTRES[0].code},${EXAMPLES.DOM.BUSINESS_CENTRES[1].code}`,
     description: 'Unique business centre codes - separated by commas',
-    // type: [String],
-    // isArray: true,
-    // type: String,
   })
   @IsString()
   public centreCodes: string;
