9106fc0e9c975986c550348505ae60c79fc1418b
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-04-28 14:38:29 +0100

    feat(GIFT-23531): update unit test

--- src/helpers/map-business-centre.test.ts
+++ src/helpers/map-business-centre.test.ts
@@ -1,19 +1,26 @@
-import { DOM_BUSINESS_CENTRES } from '@ukef/constants';
+import { EXAMPLES } from '@ukef/constants';
 
 import { mapBusinessCentre } from './map-business-centre';
 
 describe('mapBusinessCentre', () => {
   it('should return a mapped DOM business centre', () => {
     // Arrange
-    const mockBusinessCentre = DOM_BUSINESS_CENTRES.AE_DXB;
+    const mockBusinessCentre = {
+      business_centre_code: EXAMPLES.BUSINESS_CENTRE.CODE,
+      business_centre_name: EXAMPLES.BUSINESS_CENTRE.NAME,
+      business_centre_type_description: EXAMPLES.BUSINESS_CENTRE.DESCRIPTION,
+      business_centre_active_flag: EXAMPLES.BUSINESS_CENTRE.IS_ACTIVE,
+    };
 
     // Act
     const result = mapBusinessCentre(mockBusinessCentre);
 
     // Assert
     const expected = {
-      code: mockBusinessCentre.CODE,
-      name: mockBusinessCentre.NAME,
+      code: EXAMPLES.BUSINESS_CENTRE.CODE,
+      name: EXAMPLES.BUSINESS_CENTRE.NAME,
+      description: EXAMPLES.BUSINESS_CENTRE.DESCRIPTION,
+      isActive: EXAMPLES.BUSINESS_CENTRE.IS_ACTIVE,
     };
 
     expect(result).toEqual(expected);
